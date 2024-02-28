876b0a8e679a25e982cba57a08151e10a301f15e
RadislavLazarevic <62558311+RadislavLazarevic@users.noreply.github.com>
2024-02-28 01:00:32 +0100

    Add files via upload

---
+++ hzzo_spider.py
@@ -0,0 +1,42 @@
+import scrapy
+import pandas as pd
+
+class HZZOSpider(scrapy.Spider):
+    name = 'hzzo'
+    start_urls = ['https://hzzo.hr/zdravstvena-zastita/lijekovi/objavljene-liste-lijekova']
+
+    def parse(self, response):
+        # Pronađi link ka prvom Excel fajlu
+        first_excel_link = response.xpath('//a[contains(@href, ".xlsx")]/@href').extract_first()
+        self.logger.info("Link ka Excel fajlu: %s", first_excel_link)
+        yield scrapy.Request(response.urljoin(first_excel_link), callback=self.parse_excel)
+
+    def parse_excel(self, response):
+        self.logger.info("Otvaram Excel datoteku...")
+        # Otvaranje Excel datoteke
+        excel_file = pd.ExcelFile(response.body)
+
+        self.logger.info("Lista listova u Excel datoteci: %s", excel_file.sheet_names)
+
+        # Čitanje određenih listova iz Excel datoteke
+        df_oll_1 = pd.read_excel(excel_file, sheet_name='OLL-1. dio')
+        df_oll_2 = pd.read_excel(excel_file, sheet_name='OLL-2. dio')
+        df_oll_3 = pd.read_excel(excel_file, sheet_name='OLL-3. dio')
+        df_oll_4 = pd.read_excel(excel_file, sheet_name='OLL-4. dio')
+
+        self.logger.info("Prvi redovi OLL-1.dio:\n%s", df_oll_1.head())
+        self.logger.info("Prvi redovi OLL-2.dio:\n%s", df_oll_2.head())
+        self.logger.info("Prvi redovi OLL-3.dio:\n%s", df_oll_3.head())
+        self.logger.info("Prvi redovi OLL-4.dio:\n%s", df_oll_4.head())
+
+        # Spajanje svih DataFrame-ova u jedan
+        merged_df = pd.concat([df_oll_1, df_oll_2, df_oll_3, df_oll_4], ignore_index=True)
+
+        # Pretvaranje u JSON format
+        json_data = merged_df.to_json(orient='records')
+
+        # Čuvanje JSON-a u datoteku
+        with open('merged_data.json', 'w') as f:
+            f.write(json_data)
+
+        self.log('Svi listovi su uspešno spojeni i sačuvani u merged_data.json fajlu.')
